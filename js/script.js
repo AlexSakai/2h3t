971eec268101a36a5da7b726043bcb5f33e092e8
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2023-09-15 08:07:45 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 clicado = () => alert("Alô, Mundo");
+mensagem = (msg) => console.log(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+juros_s = (c,j) => mult(c,div(j,100));
+montante_js = (c,j) => soma(juros_s(c,j),c);
+
+mensagem((soma(7,soma(11,soma(15,4)))));
+
+mensagem(sub(18,7))
+
+mensagem(soma(div(2,2),2));
+
+mensagem(montante_js(420,18));  
