a59f404b3416dd109336e3a055388fbe85516da3
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2023-09-29 08:08:39 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let num1 = "";
+let num2 = "";
+let valor = "";
+let temPonto = false;
+
 clicado = () => alert("Alô, Mundo");
 mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
@@ -9,9 +14,26 @@ montante_js = (c,j) => soma(juros_s(c,j),c);
 
 mensagem((soma(7,soma(11,soma(15,4)))));
 
-mensagem(sub(18,7))
+mensagem(sub(18,7));
 
 mensagem(soma(div(2,2),2));
 
 mensagem(montante_js(420,18));  
+
+mostrar_display = (msg) => {
+    document.getElementById("resultado").value = msg;
+}
+
+function digitando(tecla){
+    if(tecla == "."){
+        if(!temPonto){
+                valor += tecla;
+                mostrar_display(valor);
+                temPonto = true;
+        }
+        return;
+    }
+    valor += tecla;
+    mostrar_display(valor);
+}
 
